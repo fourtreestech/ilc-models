1cce5ee9485a2a8cab05d1b361b59fd07eaf3bfd
Neil Martin <neilmartin12@me.com>
2025-03-15 11:22:43 +0000

    Added SquadPlayer class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import random
 import pytest
+from collections.abc import MutableSequence
+from typing import Optional, Any
 
 from faker import Faker
 from faker.providers import BaseProvider
@@ -7,6 +9,30 @@ from faker.providers import BaseProvider
 from ilc_models import BasePlayer, Lineup, Lineups, Player
 
 fake = Faker()
+
+
+class SquadPlayer:
+    """Member of a squad.
+
+    On initialization the object will be populated
+    with a randomly generated :class:`~ilc_models.BasePlayer`
+    and will be allocated two weighting attributes:
+
+        * ``selection_weight``: How likely this player is to be selected (1-100)
+        * ``scorer_weight``: How likely this player is to score a goal (1-100)
+    
+    :param shirt_number: Player's squad number
+    :type shirt_number: int
+    :param keeper: True if this player is a goalkeeper (default=False)
+    :type keeper: bool
+    """
+
+    def __init__(self, shirt_number: int, keeper=False):
+        self.shirt_number = shirt_number
+        self.keeper = keeper
+        self.base_player = fake.base_player()
+        self.selection_weight = random.randint(1, 100)
+        self.scorer_weight = 1 if keeper else random.randint(2, 100)
 
 
 class ILCProvider(BaseProvider):
@@ -66,13 +92,7 @@ class ILCProvider(BaseProvider):
         shirt_weights = [3] * 10 + [2] * 8 + [1] * 20
 
         # Generate random shirt numbers for this lineup
-        shirt_numbers: list[int] = []
-        while len(shirt_numbers) < 17:
-            n = random.choices(shirts, weights=shirt_weights)[0]
-            shirt_numbers.append(n)
-            i = shirts.index(n)
-            del shirts[i]
-            del shirt_weights[i]
+        shirt_numbers = _unique_choices(shirts, weights=shirt_weights, k=17)
 
         # Select one for the second goalkeeper
         keeper2 = random.choice([n for n in shirt_numbers if n > 11])
@@ -88,7 +108,7 @@ class ILCProvider(BaseProvider):
 
         # Get subs
         sub_shirts = [keepers[1]] + shirt_numbers[10:]
-        subs = [(shift, self.base_player()) for shift in sub_shirts]
+        subs = [(shirt, self.base_player()) for shirt in sub_shirts]
 
         return Lineup(starting=starting, subs=subs)
 
@@ -99,6 +119,35 @@ class ILCProvider(BaseProvider):
         :rtype: :class:`ilc_models.Lineups`
         """
         return Lineups(home=self.lineup(), away=self.lineup())
+
+
+def _unique_choices(
+    population: MutableSequence[Any], weights: Optional[list[int]] = None, k=1
+) -> list[Any]:
+    """Return a `k` sized list of elements chosen from the `population` without replacement.
+
+    :param population: Population to select elements from
+    :type population: MutableSequence[Any]
+    :param weights: If specified, selections are made according to the relative weights (default=None)
+    :type weights: MutableSequence[int|float]
+    :param k: Number of selections to return (default=1)
+    :type k: int
+    :returns: Selected elements
+    :rtype: list[Any]
+    """
+    _population = population[:]
+    _weights = weights[:] if weights is not None else None
+
+    choices: list[Any] = []
+    while len(choices) < k:
+        choice = random.choices(_population, weights=_weights)[0]
+        choices.append(choice)
+        i = _population.index(choice)
+        del _population[i]
+        if _weights:
+            del _weights[i]
+
+    return choices
 
 
 fake.add_provider(ILCProvider)
