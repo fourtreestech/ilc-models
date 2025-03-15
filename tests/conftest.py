2a79850ba5592952c94bcfcb80e1d0d85530b392
Neil Martin <neilmartin12@me.com>
2025-03-15 07:54:37 +0000

    Added Faker provider

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,28 @@
 import random
 import pytest
 
+from faker import Faker
+from faker.providers import BaseProvider
+
 from ilc_models import BasePlayer
 
-@pytest.fixture
-def fake_base_player(faker):
-    return BasePlayer(
-        player_id=random.randint(1, 999_999),
-        name=f"{faker.first_name()[0]}. {faker.last_name()}"
-    )
+fake = Faker()
+
+
+class ILCProvider(BaseProvider):
+    def player_id(self):
+        return random.randint(1, 999_999)
+
+    def base_player(self):
+        return BasePlayer(
+            player_id=fake.unique.player_id(),
+            name=f"{fake.first_name()[0]}. {fake.last_name()}",
+        )
+
+
+fake.add_provider(ILCProvider)
+
+
+@pytest.fixture(scope="session")
+def ilc_fake():
+    return fake
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 """Tests for data models"""
 
-class TestBasePlayer:
 
-    def test_base_player_str_returns_name(self, fake_base_player):
-        player = fake_base_player
+class TestBasePlayer:
+    def test_base_player_str_returns_name(self, ilc_fake):
+        player = ilc_fake.base_player()
         assert str(player) == player.name
