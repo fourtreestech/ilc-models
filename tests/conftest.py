2679ae8598b8213375c0d0af4e628aa61da02bc2
Neil Martin <neilmartin12@me.com>
2025-03-15 08:05:45 +0000

    Added fake_player

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from faker import Faker
 from faker.providers import BaseProvider
 
-from ilc_models import BasePlayer
+from ilc_models import BasePlayer, Player
 
 fake = Faker()
 
@@ -17,6 +17,23 @@ class ILCProvider(BaseProvider):
         return BasePlayer(
             player_id=fake.unique.player_id(),
             name=f"{fake.first_name()[0]}. {fake.last_name()}",
+        )
+
+    def player(self):
+        player_id = fake.unique.player_id()
+        first_name = fake.first_name_male()
+        last_name = fake.last_name_male()
+        name = f"{first_name[0]}. {last_name}"
+        dob = fake.date_of_birth(minimum_age=17, maximum_age=35).isoformat()
+        nationality = fake.country()
+
+        return Player(
+            player_id=player_id,
+            name=name,
+            first_name=first_name,
+            last_name=last_name,
+            dob=dob,
+            nationality=nationality,
         )
 
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,3 +5,9 @@ class TestBasePlayer:
     def test_base_player_str_returns_name(self, ilc_fake):
         player = ilc_fake.base_player()
         assert str(player) == player.name
+
+
+class TestPlayer:
+    def test_player_str_returns_name(self, ilc_fake):
+        player = ilc_fake.player()
+        assert str(player) == player.name
